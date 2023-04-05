f064b172bd1673d74199799314292d249f008f6e
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2023-04-05 14:22:27 +0200

    quest finish

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,12 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 Class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig',
         ['website' => 'Wild Series']);
+    }
+
+    #[Route('/{id}', name: 'show', requirements: ['id'=>'\d+'], methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig',
+        ['id' => $id]);
     }
 }
